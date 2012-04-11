31b35771984e9e186922c3e42e8b3e5edd282d2d
Martin Kolman <martin.kolman@gmail.com>
2012-04-11 16:13:50 +0200

    * make launch more robust

--- repho.py
+++ repho.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+import os
 import subprocess
 
 
 if __name__ == "__main__":
-  subprocess.call(["/home/user/software/repho/_repho.py", "-p", "harmattan"])
+  testingPath = "/home/user/software/repho/_repho.py"
+  if os.path.exists(testingPath):
+    print("testing path")
+    subprocess.call([testingPath, "-p", "harmattan"])
+  else:
+    print("normal path")
+    subprocess.call(["/opt/repho/_repho.py", "-p", "harmattan"])
 
